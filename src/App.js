7f3a7c76e82a1624c5ca06b88686f421bc67d025
oussamamessaoudi <oussm.messaoudi@gmail.com>
2018-12-05 23:05:26 +0100

    Calling DeltaForm Adding dependency (PropTypes)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import DeltaForm from "./container/DeltaForm";
 
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+        <DeltaForm/>
     );
   }
 }
